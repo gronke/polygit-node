ec385b686379cbe7ac92c453388cbf35139b64f1
Stefan Grönke <stefan@gronke.net>
2015-09-18 13:20:50 +0200

    caching feature

--- index.js
+++ index.js
@@ -2,34 +2,77 @@
 
 var bowerJson = require('bower-json');
 var url = require('url');
+var path = require('path');
 var cors = require('cors');
 var express = require('express');
 var app = express();
-var GitHub = require('./lib/GitHub'),
-    gitHub = new GitHub();
 
-var config = {
+// setup GitHubApi
+var GitHubApi = require('github');
+var github = new GitHubApi({
+  version: '3.0.0',
+  debug: false
+});
+
+// setup Resolver
+var Resolver = require('./lib/Resolver'),
+    resolver = new Resolver({}, github);
+
+// setup Loader
+var Loader = require('./lib/Loader'),
+    loader = new Loader({
+      debug: console.log
+    }, github, resolver);
+
+// configure
+var config;
+config = {
   port: process.env.PORT || 3333
 };
 
+// serve demo as /
+app.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname, 'demo/demo.html'));
+});
+
+app.get('/demo.css', function(req, res) {
+  res.sendfile(path.join(__dirname, 'demo/demo.css'));
+});
+
+app.use('/bower_components', express.static('demo/fake_components'));
+
+// allow CORS
 app.use(cors({
   allowedOrigins: ['*']
 }));
 
-var proxy = require('express-http-proxy');
+app.get('/*', function(req, res) {
+  var endpoint = parseUrl(req.url);
+  var registryComponent = resolver.lookup(endpoint.path);
 
-app.get('/', express.static('demo.html'));
-
-app.get('*', proxy('https://raw.githubusercontent.com', {
-  filter: function(req, res) {
-     return req.method == 'GET';
-  },
-  forwardPath: function(req, res) {
-    var endpoint = parseUrl(req.url);
-    var targetUrl = '/' + gitHub.getFullPath(endpoint);
-    return targetUrl;
+  if(endpoint.path.indexOf('bower_components') > 0) {
+    console.log('ENDPOINT failed', endpoint.name);
+    res.sendStatus(404);
+    res.end();
   }
-}));
+
+  if(!registryComponent) {
+    res.redirect(302, path.join('../bower_components', endpoint.path));
+    res.end();
+    return;
+  }
+
+  loader.load(endpoint)
+  .then(function(fileContent) {
+    res.send(fileContent);
+    res.end();
+  })
+  .catch(function(e) {
+    res.sendStatus(404);
+    res.end();
+  });
+
+});
 
 app.listen(config.port);
 
@@ -41,13 +84,12 @@ function parseUrl(value, base) {
     pathname = pathname.slice(pathname.indexOf('base'));
   }
 
-  var paths = pathname.slice(1).split('/');
-  var componentName = paths.splice(0, 2).join('/');
-
-  var file = paths.join('/');
-  return {
-    name: componentName,
-    version: 'master',
-    file: file
+  if(pathname.indexOf('/')===0) {
+    pathname = pathname.slice(1);
   }
+
+  return {
+    path: pathname,
+    version: 'master'
+  };
 }
